0cd23666953f63e04fb8ffb5b5c3bab4f9e80ae4
Dayne Daylong <dalexad@gmail.com>
2020-01-21 15:06:18 -0800

    added test classes

---
+++ src/test/java/com/daylong/songr/HomeControllerTest.java
@@ -0,0 +1,21 @@
+package com.daylong.songr;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class HomeControllerTest {
+
+    @Test
+    void getHomePage() {
+    }
+
+    @Test
+    void capitalize() {
+    }
+
+    @Test
+    void getAlbums() {
+    }
+}
---
+++ src/test/java/com/daylong/songr/SongrApplicationTest.java
@@ -0,0 +1,13 @@
+package com.daylong.songr;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class SongrApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
