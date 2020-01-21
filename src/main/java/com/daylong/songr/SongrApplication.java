1ddd963ec1b9f986468d388d5715238903fbc421
Dayne Daylong <dalexad@gmail.com>
2020-01-21 10:29:13 -0800

    initial commit from Spring init

---
+++ src/main/java/com/daylong/songr/SongrApplication.java
@@ -0,0 +1,15 @@
+package com.daylong.songr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongrApplication.class, args);
+	}
+
+}
+
+
