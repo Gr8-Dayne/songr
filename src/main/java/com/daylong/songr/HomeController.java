7036ff1fb59d02a9ed75d39dd8754d18b7ba6a76
Dayne Daylong <dalexad@gmail.com>
2020-01-21 17:03:20 -0800

    notes from lecture 12

--- src/main/java/com/daylong/songr/HomeController.java
+++ src/main/java/com/daylong/songr/HomeController.java
@@ -1,14 +1,23 @@
 package com.daylong.songr;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
 
 
 @Controller public class HomeController {
+
+    // This connects App to postgres. Allows us to CRUD
+    @Autowired
+    AlbumRepo albumRepo;
 
     @GetMapping("/") public String getHomePage(@RequestParam(defaultValue = "GENERAL KANOBI", required=false) String user, Model m){
         m.addAttribute("user", user);
@@ -24,12 +33,25 @@ import org.springframework.web.bind.annotation.RequestParam;
     }
 
     @GetMapping("/album") public String getAlbums(Model m){
-        Album[] daftAlbums = new Album[]{
-                new Album("Random Access Memories", "Daft Punk", 13, 74.24, "https://img.discogs.com/FU2NGUXkaKMzJZ8queTYGbylB4U=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-4570505-1368699003-9153.jpeg.jpg"),
-                new Album("Human After All", "Daft Punk", 10, 45.38, "https://upload.wikimedia.org/wikipedia/en/thumb/0/0d/Humanafterall.jpg/220px-Humanafterall.jpg"),
-                new Album( "Homework", "Daft Punk", 16, 73.53, "https://static.raru.co.za/cover/2014/05/03/135503-m.jpg?v=1399102231")
-        };
+
+//        Album[] daftAlbums = new Album[]{
+//                new Album("Random Access Memories", "Daft Punk", 13, 74.24, "https://img.discogs.com/FU2NGUXkaKMzJZ8queTYGbylB4U=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-4570505-1368699003-9153.jpeg.jpg"),
+//                new Album("Human After All", "Daft Punk", 10, 45.38, "https://upload.wikimedia.org/wikipedia/en/thumb/0/0d/Humanafterall.jpg/220px-Humanafterall.jpg"),
+//                new Album( "Homework", "Daft Punk", 16, 73.53, "https://static.raru.co.za/cover/2014/05/03/135503-m.jpg?v=1399102231")
+//        };
+
+        List<Album> daftAlbums = albumRepo.findAll();
+
         m.addAttribute("albumsPunk", daftAlbums);
         return "albums";
+    }
+
+    @PostMapping("/albums") public RedirectView postAlbums(String albumTitle, String albumArtist, int albumsSongs, double albumLength, String albumCover){
+
+        Album newAlbum = new Album(albumTitle, albumArtist, albumsSongs, albumLength, albumCover);
+
+        albumRepo.save(newAlbum);
+
+        return new RedirectView("/albums");
     }
 }
