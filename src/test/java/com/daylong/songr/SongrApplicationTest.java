7a4da374f12d5b4dd9f8dc12c94070d43eb87373
Dayne Daylong <dalexad@gmail.com>
2020-01-22 16:58:44 -0800

    more notes

--- src/test/java/com/daylong/songr/SongrApplicationTest.java
+++ src/test/java/com/daylong/songr/SongrApplicationTest.java
@@ -2,12 +2,27 @@ package com.daylong.songr;
 
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.context.WebApplicationContext;
 
 
+@SpringBootTest
 class SongrApplicationTest {
 
+    @Autowired
+    private WebApplicationContext wac;
+
     @Test
-    void main() {
+    void contextLoads() {
+    }
+
+    @ExtendWith(SpringExtension.class)
+    @ContextConfiguration(classes = { SongrApplicationTest.class })
+    public class GreetingsSpringTest {
+
     }
 }
